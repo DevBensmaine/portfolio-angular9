ec1c73b70a25d65440bfa2338df00f9e9c49eb25
DevBensmaine <bensmaine.hassan@gmail.com>
2020-06-28 01:39:28 +0100

    create all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+
+import { RouterModule } from '@angular/router';
+import { ContainerComponent } from './container/container.component';
+import { AproposComponent } from './apropos/apropos.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
+import { FormationsComponent } from './formations/formations.component';
+import { InteretsComponent } from './interets/interets.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { CompetencesComponent } from './competences/competences.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    ContainerComponent,
+    AproposComponent,
+    ExperiencesComponent,
+    FormationsComponent,
+    InteretsComponent,
+    CertificationsComponent,
+    NotfoundComponent,
+    CompetencesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path :'apropos',component:AproposComponent },
+      { path :'experiences',component:ExperiencesComponent },
+      { path :'formations',component:FormationsComponent },
+      { path :'competences',component:CompetencesComponent },
+      { path :'interet',component:InteretsComponent },
+      { path :'certification',component:CertificationsComponent },
+      { path :'notfound',component:NotfoundComponent },
+      { path :'',redirectTo:'apropos' , pathMatch:'full' },
+      { path :'**',redirectTo:'notfound' , pathMatch:'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
